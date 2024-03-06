ad8f151713acc5765bd9f3007ea7b7e6fa948878
Masayuki Onishi <47713499+masayuki-0319@users.noreply.github.com>
2024-03-06 16:39:32 +0900

    delete vitest code

--- vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  test: {
-    globals: true,  // Don't forget!
-    environment: "vprisma",
-    setupFiles: ["vitest-environment-vprisma/setup", "vitest.setup.ts"]
-  },
-});
--- vitest.setup.ts
+++
@@ -1,5 +0,0 @@
-import { vi } from "vitest";
-
-vi.mock("./app/db.server", () => ({
-  prisma: vPrisma.client,
-}));
