400e2f236d6489a5dcea5f04c99e5bcb0824db27
Masayuki Onishi <47713499+masayuki-0319@users.noreply.github.com>
2024-03-19 12:25:43 +0900

    DB に初期データ追加可能にする

---
+++ prisma/seed/index.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from '@prisma/client';
+import { postRepository } from '~/models/post.server';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await postRepository.create({
+    title: 'First Post',
+    content: 'This is a first post',
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
